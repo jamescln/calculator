151529117ad0db8acf9db58b144ba2d71d08b571
James <james.jcarline@gmail.com>
2022-11-21 15:02:16 +0000

    Remove the ability to divide by 0

--- main.js
+++ main.js
@@ -25,7 +25,11 @@ const multiplyNumbers = function (num1, num2) {
 };
 
 const divideNumbers = function (num1, num2) {
-  return Number(num1) / Number(num2);
+  if (num1 === "0" || num2 === "0") {
+    return "Nice Try.";
+  } else {
+    return Number(num1) / Number(num2);
+  }
 };
 
 const operate = function (operator, num1, num2) {
